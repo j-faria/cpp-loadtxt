7ca6a82a8e8b24ef0ee46626777caf56be01173a
João Faria <joao.faria@unige.ch>
2025-03-10 17:55:06 +0100

    more tests

--- src/loadtxt.cpp
+++ src/loadtxt.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main() {
     { // test 1
-        auto fname = "data.txt";
+        std::string fname = "data.txt";
+        std::ifstream f(fname);
         auto data = loadtxt(fname).comments("#")();
-        printf("Read %ld columns, each with %ld rows from file '%s'\n", 
-               data.size(), data[0].size(), fname);
+        std::cout << "Read " << data.size() << " columns, each with " << data[0].size() << " rows from file '" << fname << "'\n";
 
         assert(data.size() == 4 && "should have read 4 columns");
         assert(data[0].size() == 100 && "should have read 100 rows");
@@ -18,9 +18,8 @@ int main() {
     }
     { // test 2
         auto fname = "data.rdb";
-        auto data = loadrdb(fname)();
-        printf("Read %ld columns, each with %ld rows from file '%s'\n", 
-               data.size(), data[0].size(), fname);
+        auto data = loadtxt(fname).skiprows(2)();
+        std::cout << "Read " << data.size() << " columns, each with " << data[0].size() << " rows from file '" << fname << "'\n";
 
         assert(data.size() == 3 && "should have read 3 columns");
         assert(data[0].size() == 5 && "should have read 5 rows");
@@ -29,26 +28,48 @@ int main() {
     }
     { // test 3
         auto fname = "data.txt";
-        auto data = loadrdb(fname).skiprows(10).comments("#")();
-        printf("Read %ld columns, each with %ld rows from file '%s'\n", 
-               data.size(), data[0].size(), fname);
+        auto data = loadtxt(fname).skiprows(10).comments("#")();
+        std::cout << "Read " << data.size() << " columns, each with " << data[0].size() << " rows from file '" << fname << "'\n";
 
         assert(data.size() == 4 && "should have read 4 columns");
         assert(data[0].size() == 91 && "should have read 91 rows");
         assert(data[1].size() == 91 && "should have read 91 rows");
         assert(data[2].size() == 91 && "should have read 91 rows");
+        assert(data[3].size() == 91 && "should have read 91 rows");
     }
     { // test 4
         auto fname = "data.txt";
-        auto data = loadrdb(fname).skiprows(10).max_rows(8).comments("#")();
-        printf("Read %ld columns, each with %ld rows from file '%s'\n", 
-               data.size(), data[0].size(), fname);
+        auto data = loadtxt(fname).skiprows(10).max_rows(8).comments("#")();
+        std::cout << "Read " << data.size() << " columns, each with " << data[0].size() << " rows from file '" << fname << "'\n";
 
         assert(data.size() == 4 && "should have read 4 columns");
         assert(data[0].size() == 8 && "should have read 8 rows");
         assert(data[1].size() == 8 && "should have read 8 rows");
         assert(data[2].size() == 8 && "should have read 8 rows");
+        assert(data[3].size() == 8 && "should have read 8 rows");
     }
-    printf("Done reading!\n");
+    { // test 5
+        auto fname = "data.csv";
+        auto data = loadtxt(fname)();
+        std::cout << "Read " << data.size() << " columns, each with " << data[0].size() << " rows from file '" << fname << "'\n";
+
+        assert(data.size() == 4 && "should have read 4 columns");
+        assert(data[0].size() == 8 && "should have read 8 rows");
+        assert(data[1].size() == 8 && "should have read 8 rows");
+        assert(data[2].size() == 8 && "should have read 8 rows");
+        assert(data[3].size() == 8 && "should have read 8 rows");
+    }
+    { // test 6
+        auto fname = "data.tsv";
+        auto data = loadtxt(fname)();
+        std::cout << "Read " << data.size() << " columns, each with " << data[0].size() << " rows from file '" << fname << "'\n";
+
+        assert(data.size() == 3 && "should have read 3 columns");
+        assert(data[0].size() == 20 && "should have read 20 rows");
+        assert(data[1].size() == 20 && "should have read 20 rows");
+        assert(data[2].size() == 20 && "should have read 20 rows");
+    }
+
+    std::cout << "Done reading!\n";
     return 0;
 }
